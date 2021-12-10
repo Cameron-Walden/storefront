5827ba020948373f2e5901835b3c00ff463d5ef0
Cameron <cameronkwalden@gmail.com>
2021-12-09 17:31:29 -0800

    can add items to cart

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,19 @@
-const Header = () => {
+import { connect } from 'react-redux';
+
+const Header = props => {
 
   return (
+    <>
     <h1>Storefront</h1>
+    <p>Cart: {props.cart.cart.length}</p>
+    </>
   )
 }
 
-export default Header;
+const mapStateToProps = state => {
+  return {
+    cart: state.cart
+  }
+}
+
+export default connect(mapStateToProps)(Header);
